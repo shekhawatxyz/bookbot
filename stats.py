cfe6cd2732078c54c147c7104ed4da7aa265f48d
Parakram Singh Shekhawat <parakram.shekhawat@gmail.com>
2025-03-12 13:49:18 +0530

    first project - bookbot

---
+++ stats.py
@@ -0,0 +1,24 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def count(text):
+    text = text.lower()
+    dic={}
+    for key in text:
+        if key.isalpha():
+            if key in dic:
+                dic[key] += 1
+            else:
+                dic[key] = 1
+
+    return dict(sorted(dic.items()))
+
+# A function that takes a dictionary and returns the value of the "num" key
+# This is how the `.sort()` method knows how to sort the list of dictionaries
+def sort_on(char_count):
+    char_count= sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+    return char_count
+
+
+# [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]

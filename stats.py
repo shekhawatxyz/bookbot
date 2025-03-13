bae6235f90827da62d82fc000ba7cdf156f600e5
Alexander Nicholi <me@nicholatian.com>
2025-03-13 18:04:54 -0400

    various minor fixes

--- stats.py
+++ stats.py
@@ -4,21 +4,21 @@ def get_num_words(text):
 
 def count(text):
     text = text.lower()
-    dic={}
+    dic = {}
     for key in text:
-        if key.isalpha():
-            if key in dic:
-                dic[key] += 1
-            else:
-                dic[key] = 1
-
+        if not key.isalpha():
+            continue
+        if key in dic:
+            dic[key] += 1
+        else:
+            dic[key] = 1
     return dict(sorted(dic.items()))
 
-# A function that takes a dictionary and returns the value of the "num" key
-# This is how the `.sort()` method knows how to sort the list of dictionaries
+# A function that takes a dictionary and returns the value of the "num"
+# key This is how the `.sort()` method knows how to sort the list of
+# dictionaries
 def sort_on(char_count):
-    char_count= sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+    char_count = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
     return char_count
-
 
 # [{'name': 'plane', 'num': 10}, {'name': 'car', 'num': 7}, {'name': 'boat', 'num': 2}]
